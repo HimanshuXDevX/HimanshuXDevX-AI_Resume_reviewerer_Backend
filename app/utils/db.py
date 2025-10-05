4261a4ecca04bd2ff1e2fec26f8f3bc77e43b6b7
Himanshu__808 <55khimanshu@gmail.com>
2025-10-05 19:46:18 +0530

    bug fix for event loop is closed

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from app.models.user import User
@@ -11,26 +10,25 @@ load_dotenv()
 logger = logging.getLogger("uvicorn.error")
 
 _db_initialized = False
-_db_lock = asyncio.Lock()
 _client = None
 
 async def init_db():
     global _db_initialized, _client
-    async with _db_lock:
-        if _db_initialized:
-            return
 
-        mongo_uri = os.getenv("MONGO_URI")
-        db_name = os.getenv("DB_NAME")
-        if not mongo_uri or not db_name:
-            raise ValueError("Missing MONGO_URI or DB_NAME in environment variables")
+    if _db_initialized:
+        return
 
-        logger.info("Connecting to MongoDB...")
-        _client = AsyncIOMotorClient(mongo_uri)
-        db = _client[db_name]
+    mongo_uri = os.getenv("MONGO_URI")
+    db_name = os.getenv("DB_NAME")
+    if not mongo_uri or not db_name:
+        raise ValueError("Missing MONGO_URI or DB_NAME in environment variables")
 
-        logger.info("Initializing Beanie with models...")
-        await init_beanie(database=db, document_models=[User])
+    logger.info("Connecting to MongoDB...")
+    _client = AsyncIOMotorClient(mongo_uri)
+    db = _client[db_name]
 
-        _db_initialized = True
-        logger.info("Database initialized successfully.")
+    logger.info("Initializing Beanie with models...")
+    await init_beanie(database=db, document_models=[User])
+
+    _db_initialized = True
+    logger.info("Database initialized successfully.")
